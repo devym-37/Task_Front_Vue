d0d043f3c667213680e4f613ddd5a52d26576c41
LeeYoungMin <miniyoung37@gmail.com>
2020-02-06 01:54:13 +0900

    fix app.vue

--- task_front_assignment/src/App.vue
+++ task_front_assignment/src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <v-app class="grey lighten-4">
     <v-content class="mx-4 mb-4">
-      <!-- <v-toolbar flat app>
-        <v-toolbar-title class="text-uppercase">
-          <span>Inventory </span>
-          <span class="font-weight-light"> List</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn flat color="grey">
-          <span>Sign Out</span>
-        </v-btn>
-      </v-toolbar> -->
       <Navigationbar />
       <router-view></router-view>
     </v-content>
